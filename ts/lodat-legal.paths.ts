8984b63db7fb69ca747233dd1bfff6488f6a537e
Phil Kunz <gitlab@philkunz.com>
2020-01-18 18:07:09 +0000

    fix(core): update

--- ts/lodat-legal.paths.ts
+++ ts/lodat-legal.paths.ts
@@ -1,6 +1,6 @@
 import * as plugins from './lodat-legal.plugins';
 
 export let packageDir = plugins.path.join(__dirname, '../');
-export let docsDir = plugins.path.join(packageDir, 'docs/');
-export let privacyPolicyDir = plugins.path.join(docsDir, 'privacypolicy');
-export let cookiePolicyDir = plugins.path.join(docsDir, 'cookiepolicy');
+export let assetsDir = plugins.path.join(packageDir, 'assets/');
+export let privacyPolicyDir = plugins.path.join(assetsDir, 'privacypolicy');
+export let cookiePolicyDir = plugins.path.join(assetsDir, 'cookiepolicy');
